4d8384cc72b53ff6c9fbd77df33aa696b37244e8
Jakub Okrasa <j.m.okrasa@gmail.com>
2019-04-16 18:16:28 +0100

    initial commit

---
+++ src/test/java/com/jtm/notesapp/NotesAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jtm.notesapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotesAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
