9942d4241b023bea026666d332393f16c7b24ab2
Jakub Okrasa <j.m.okrasa@gmail.com>
2019-04-29 19:23:45 +0100

    Don't push | edit and delete don't work. Delete doesnt work even in NoteController

--- src/main/java/com/jtm/notesapp/controllers/HomeController.java
+++ src/main/java/com/jtm/notesapp/controllers/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -32,5 +33,22 @@ public class HomeController {
     public String addNote(@ModelAttribute NoteDto note) {
         noteService.addNote(note);
         return "redirect:/";
+    }
+
+    @GetMapping("/delete")
+    public String deleteNote(@RequestParam String noteTitle) {
+        noteService.deleteNotesByTitle(noteTitle);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit")
+    public String getEditPage() {
+        return "redirect:/";
+    }
+
+    @PostMapping("/edit")
+    public  String editNote(@ModelAttribute NoteDto editedNote) {
+        noteService.updateNoteByNoteTitle(editedNote);
+        return "edit";
     }
 }
